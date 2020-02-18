0ffb59381d6762d6245c4f306a5a0f44c5499aae
Tomasz Borychowski <tomasz.borychowski@sap.com>
2020-02-18 11:55:08 +0000

    update FAQ link

--- src/app/index.svelte
+++ src/app/index.svelte
@@ -13,7 +13,7 @@
 		Unfortunately, due to Firefox security restrictions,<br>
 		Perfect Home is unable to open links to local files.<br><br>
 		You can change that yourself in your browser settings.<br>
-		See <a href="https://github.com/perfect-things/perfect-home/blob/master/FAQ.md" target="_blank">FAQ</a> on how to do that.<br>
+		See <a href="https://github.com/perfect-things/perfect-home/blob/master/FAQ.md#cant-open-a-bookmark-to-a-local-file-starting-with-file" target="_blank">FAQ</a> on how to do that.<br>
 		Your extension URL is:<br>
 		<input class="ext-url" type="text" readonly value="{location.href}" on:focus="{e => e.target.select()}">
 	</p>
